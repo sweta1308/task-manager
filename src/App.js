a2d5de6ba4dc081747fdc7ed5afacd8d6e617e97
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-12 11:26:43 +0530

    feat: add Home component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Home } from "./pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App px-[40px] py-[30px]">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
