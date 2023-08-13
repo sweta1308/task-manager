3d0636b5a3544d33054b188c737d15cfe6c4b2d6
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-13 13:21:23 +0530

    feat: add testing component

---
+++ src/components/Testing.jsx
@@ -0,0 +1,22 @@
+import { useStatus } from "../context/statusContext";
+import { TaskCard } from "./TaskCard";
+
+export const Testing = () => {
+  const { statusTesting } = useStatus();
+  return (
+    <div>
+      <h2 className="border-b-4 border-orange-400 text-left pb-[15px] font-bold">
+        TESTING{" "}
+        <span className="text-gray-500 text-[12px] ">
+          ({statusTesting?.length})
+        </span>
+      </h2>
+
+      {statusTesting?.map((testing) => (
+        <div key={testing?.id}>
+          <TaskCard task={testing} />
+        </div>
+      ))}
+    </div>
+  );
+};
