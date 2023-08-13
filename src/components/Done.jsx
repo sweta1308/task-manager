65c92d036c533ffe27291d0d57395d0a4cc933fd
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-13 13:19:22 +0530

    feat: add done component

---
+++ src/components/Done.jsx
@@ -0,0 +1,22 @@
+import { useStatus } from "../context/statusContext";
+import { TaskCard } from "./TaskCard";
+
+export const Done = () => {
+  const { statusDone } = useStatus();
+  return (
+    <div>
+      <h2 className="border-b-4 border-green-400 text-left pb-[15px] font-bold">
+        DONE{" "}
+        <span className="text-gray-500 text-[12px] ">
+          ({statusDone?.length})
+        </span>
+      </h2>
+
+      {statusDone?.map((done) => (
+        <div key={done?.id}>
+          <TaskCard task={done} />
+        </div>
+      ))}
+    </div>
+  );
+};
