340678ef55a112a4a1225aabcfe4bed6d73542f3
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-13 13:38:40 +0530

    refactor: remove console

--- src/context/statusContext.js
+++ src/context/statusContext.js
@@ -6,19 +6,6 @@ const StatusContext = createContext();
 export const StatusProvider = ({ children }) => {
   const { tasks } = useTask();
 
-  //   const getStatus = (arr) => {
-  //     const status = {};
-  //     for (let obj of arr) {
-  //       if (status[obj.status]) {
-  //         return status;
-  //       } else {
-  //         status[obj.status] = obj;
-  //       }
-  //     }
-  //     return status;
-  //   };
-
-  //   const statusArr = Object.keys(getStatus(tasks));
   const statusReady = tasks?.filter((task) => task.status === "Ready");
   const statusProgress = tasks?.filter((task) => task.status === "In Progress");
   const statusTesting = tasks?.filter((task) => task.status === "Testing");
