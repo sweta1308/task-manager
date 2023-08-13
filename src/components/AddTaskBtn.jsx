22fd164fa295ad03a24048a3ab8ffcf8ccd9ea0f
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-13 13:37:46 +0530

    docs: add task button

---
+++ src/components/AddTaskBtn.jsx
@@ -0,0 +1,9 @@
+export const AddTask = () => {
+  return (
+    <div>
+      <button className="text-[#10b981] font-bold rounded-sm w-[350px] bg-white shadow-md py-[10px] text-[18px]">
+        <i className="fa-solid fa-plus"></i> Add Task
+      </button>
+    </div>
+  );
+};
