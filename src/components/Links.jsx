e95aac878f15bf63920476219012e0925d438a7f
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-13 13:20:29 +0530

    feat: add Links component

---
+++ src/components/Links.jsx
@@ -0,0 +1,15 @@
+export const Links = () => {
+  return (
+    <>
+      <div className="flex w-[700px] justify-between mt-[40px] text-[18px] text-gray-500">
+        <p>Overview</p>
+        <p className="text-black border-b-4 border-[#10b981] font-semibold">Tasks</p>
+        <p>Milestones</p>
+        <p>Timesheets</p>
+        <p>Files</p>
+        <p>Discussions</p>
+        <p>Activity Feed</p>
+      </div>
+    </>
+  );
+};
