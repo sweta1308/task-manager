3e79d61e9367c2a948adafea77ee03a9def11948
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-14 15:16:47 +0530

    feat: add testing list

--- src/components/Testing.jsx
+++ src/components/Testing.jsx
@@ -1,25 +1,34 @@
+import { Droppable } from "react-beautiful-dnd";
 import { useStatus } from "../context/statusContext";
-import { AddTask } from "./AddTaskBtn";
 import { TaskCard } from "./TaskCard";
 
 export const Testing = () => {
   const { statusTesting } = useStatus();
   return (
-    <div>
-      <h2 className="border-b-4 border-orange-400 text-left pb-[15px] font-bold">
-        TESTING{" "}
-        <span className="text-gray-500 text-[12px] ">
-          ({statusTesting?.length})
-        </span>
-      </h2>
+    <Droppable droppableId="TestingTasks">
+      {(provided, snapshot) => (
+        <div
+          className={`w-[350px] ${
+            snapshot.isDraggingOver ? "bg-gray-300" : ""
+          } rounded-md`}
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+        >
+          <h2 className="border-b-4 border-orange-400 text-left pb-[15px] font-bold">
+            TESTING{" "}
+            <span className="text-gray-500 text-[12px] ">
+              ({statusTesting?.length})
+            </span>
+          </h2>
 
-      {statusTesting?.map((testing) => (
-        <div key={testing?.id}>
-          <TaskCard task={testing} />
+          {statusTesting?.map((testing, index) => (
+            <div key={testing?.id}>
+              <TaskCard task={testing} index={index} />
+            </div>
+          ))}
+          {provided.placeholder}
         </div>
-      ))}
-
-      <AddTask />
-    </div>
+      )}
+    </Droppable>
   );
 };
