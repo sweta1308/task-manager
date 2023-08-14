889894436b450f89d63b00bd78ac8c7bfe01c5ae
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-14 17:15:55 +0530

    style: add dark mode color

--- src/components/Testing.jsx
+++ src/components/Testing.jsx
@@ -8,9 +8,9 @@ export const Testing = () => {
     <Droppable droppableId="TestingTasks">
       {(provided, snapshot) => (
         <div
-          className={`w-[350px] ${
-            snapshot.isDraggingOver ? "bg-gray-300" : ""
-          } rounded-md`}
+          className={`w-[350px] xs:w-[300px] ${
+            snapshot.isDraggingOver ? "bg-gray-300 dark:bg-dark-light" : ""
+          } rounded-md pb-[40px]`}
           ref={provided.innerRef}
           {...provided.droppableProps}
         >
@@ -20,6 +20,10 @@ export const Testing = () => {
               ({statusTesting?.length})
             </span>
           </h2>
+
+          {statusTesting?.length === 0 && (
+            <h1 className="font-bold text-[20px] mt-[20px]">No tasks added.</h1>
+          )}
 
           {statusTesting?.map((testing, index) => (
             <div key={testing?.id}>
