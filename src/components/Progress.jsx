c8b75da8549ada9c93de297b78e4e9cdc76abb2d
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-14 15:15:46 +0530

    feat: add in progress list

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,25 +1,34 @@
+import { Droppable } from "react-beautiful-dnd";
 import { useStatus } from "../context/statusContext";
-import { AddTask } from "./AddTaskBtn";
 import { TaskCard } from "./TaskCard";
 
 export const Progress = () => {
   const { statusProgress } = useStatus();
   return (
-    <div>
-      <h2 className="border-b-4 border-yellow-400 text-left pb-[15px] font-bold">
-        IN PROGRESS{" "}
-        <span className="text-gray-500 text-[12px] ">
-          ({statusProgress?.length})
-        </span>
-      </h2>
+    <Droppable droppableId="ProgressTasks">
+      {(provided, snapshot) => (
+        <div
+          className={`w-[350px] ${
+            snapshot.isDraggingOver ? "bg-gray-300" : ""
+          } rounded-md`}
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+        >
+          <h2 className="border-b-4 border-yellow-400 text-left pb-[15px] font-bold">
+            IN PROGRESS{" "}
+            <span className="text-gray-500 text-[12px] ">
+              ({statusProgress?.length})
+            </span>
+          </h2>
 
-      {statusProgress?.map((progress) => (
-        <div key={progress?.id}>
-          <TaskCard task={progress} />
+          {statusProgress?.map((progress, index) => (
+            <div key={progress?.id}>
+              <TaskCard task={progress} index={index} />
+            </div>
+          ))}
+          {provided.placeholder}
         </div>
-      ))}
-
-      <AddTask />
-    </div>
+      )}
+    </Droppable>
   );
 };
