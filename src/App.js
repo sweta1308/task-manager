5164acb7b9dbc0aa67c8faf9e6b38cae76c5cce0
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-14 17:14:51 +0530

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import { Routes, Route } from "react-router";
 import { Home } from "./pages/Home";
+import { Metrics } from "./pages/Metrics";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
+    <div className="App dark:bg-dark-mode dark:text-white">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/metrics" element={<Metrics />} />
+      </Routes>
     </div>
   );
 }
