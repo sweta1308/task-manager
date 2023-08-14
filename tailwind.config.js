95c071833112455d1923a0260527b79e539dc1ff
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-14 15:18:06 +0530

    docs: add screens for responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
+    screens: {
+      "2xl": { max: "1500px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "670px" },
+      // => @media (max-width: 639px) { ... }
+      xs: { max: "470px" },
+    },
     extend: {},
   },
   plugins: [],
