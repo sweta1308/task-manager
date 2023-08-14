9d8cd51ff0472631f0859031f753517ea6fb2dc0
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-14 17:15:32 +0530

    feat: add links

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,19 +1,19 @@
+import { NavLink } from "react-router-dom";
+
 export const Links = () => {
+  const getStyles = ({ isActive }) => ({
+    borderBottom: isActive ? "5px solid #10b981" : "",
+    fontWeight: isActive ? "600" : "",
+  });
   return (
     <>
-      <div className="flex w-[700px] justify-between mt-[40px] text-[18px] text-gray-500 md:w-[600px] md:text-[16px] sm:hidden">
-        <p>Overview</p>
-        <p className="text-black border-b-4 border-[#10b981] font-semibold">
+      <div className="flex w-[200px] justify-between mt-[40px] text-[18px] px-[20px]">
+        <NavLink to="/" style={getStyles}>
           Tasks
-        </p>
-        <p>Milestones</p>
-        <p>Timesheets</p>
-        <p>Files</p>
-        <p>Discussions</p>
-        <p>Activity Feed</p>
-      </div>
-      <div className="text-black border-b-4 border-[#10b981] font-semibold text-[20px] hidden sm:block mt-[30px]">
-        Tasks
+        </NavLink>
+        <NavLink to="/metrics" style={getStyles}>
+          Metrics
+        </NavLink>
       </div>
     </>
   );
