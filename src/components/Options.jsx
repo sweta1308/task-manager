11ae2c8159c2f2757cc2dded683e46d2eaa08d66
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-14 15:15:33 +0530

    feat: add filter functionalities

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,33 +1,68 @@
+import { useTask } from "../context/taskContext";
+
 export const Options = () => {
+  const {
+    searchInput,
+    setSearchInput,
+    selectedType,
+    setSelectedType,
+    priority,
+    setPriority,
+  } = useTask();
   return (
     <>
-      <div className="flex justify-between py-[20px]">
-        <div className="flex">
-          <p className="text-[14px] bg-white items-center mr-[20px] border-4 p-[10px]">
-            <i className="fa-solid fa-plus"></i> Add Task
-          </p>
-          <p className="text-[14px] bg-white items-center border-4 p-[10px] relative">
-            <i className="fa-solid fa-filter text-[12px]"></i> Add Filter{" "}
-            <i className="fa-solid fa-sort-down relative bottom-1"></i>
-          </p>
+      <div className="flex justify-between py-[20px] md:flex-col md:items-center md:gap-3">
+        <div className="flex items-center xs:text-[12px] xs:flex-col">
+          <input
+            placeholder={`Search by ${selectedType}`}
+            className="p-[10px] mr-[10px] outline-none"
+            value={searchInput}
+            onChange={(e) => {
+              setSearchInput(e.target.value);
+            }}
+          />
+          <div className="xs:mt-[10px]">
+            <label for="name">
+              <input
+                id="name"
+                type="radio"
+                name="type"
+                value="name"
+                checked={selectedType === "name"}
+                onChange={(e) => setSelectedType(e.target.value)}
+              />{" "}
+              Name
+            </label>
+            <label for="assignee" className="m-[10px]">
+              <input
+                id="assignee"
+                type="radio"
+                name="type"
+                value="assignee"
+                checked={selectedType === "assignee"}
+                onChange={(e) => setSelectedType(e.target.value)}
+              />{" "}
+              Assignee
+            </label>
+          </div>
         </div>
 
-        <div className="flex text-[14px] border-2 border-gray-300 rounded-sm font-semibold">
+        <div className="flex text-[14px] border-2 border-gray-300 rounded-sm font-semibold md:w-fit">
           <p className="p-[10px]">Ungrouped</p>
           <p className="p-[10px] bg-white">Users</p>
         </div>
 
-        <div className="flex">
-          <p className="text-[14px] bg-white items-center border-4 p-[10px] relative mr-[20px]">
-            <i className="fa-solid fa-gear"></i> More{" "}
-            <i className="fa-solid fa-sort-down relative bottom-1"></i>
-          </p>
-          <p className="bg-white p-[10px] border-r-2">
-            <i className="fa-solid fa-pause text-[#10b981]"></i>
-          </p>
-          <p className="bg-white p-[10px]">
-            Board <i className="fa-solid fa-sort-down relative bottom-1"></i>
-          </p>
+        <div className="flex md:flex-col md:w-fit">
+          <select
+            className="px-[10px] md:py-[10px]"
+            value={priority}
+            onChange={(e) => setPriority(e.target.value)}
+          >
+            <option value="">Select for Priority</option>
+            <option value="High">High</option>
+            <option value="Medium">Medium</option>
+            <option value="Low">Low</option>
+          </select>
         </div>
       </div>
     </>
