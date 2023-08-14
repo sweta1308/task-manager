db179ef222062d3b3dcb87a11b08e232228eda6c
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-14 15:14:49 +0530

    feat: add done list

--- src/components/Done.jsx
+++ src/components/Done.jsx
@@ -1,25 +1,34 @@
+import { Droppable } from "react-beautiful-dnd";
 import { useStatus } from "../context/statusContext";
-import { AddTask } from "./AddTaskBtn";
 import { TaskCard } from "./TaskCard";
 
 export const Done = () => {
   const { statusDone } = useStatus();
   return (
-    <div>
-      <h2 className="border-b-4 border-green-400 text-left pb-[15px] font-bold">
-        DONE{" "}
-        <span className="text-gray-500 text-[12px] ">
-          ({statusDone?.length})
-        </span>
-      </h2>
+    <Droppable droppableId="DoneTasks">
+      {(provided, snapshot) => (
+        <div
+          className={`w-[350px] ${
+            snapshot.isDraggingOver ? "bg-gray-300" : ""
+          } rounded-md`}
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+        >
+          <h2 className="border-b-4 border-green-400 text-left pb-[15px] font-bold">
+            DONE{" "}
+            <span className="text-gray-500 text-[12px] ">
+              ({statusDone?.length})
+            </span>
+          </h2>
 
-      {statusDone?.map((done) => (
-        <div key={done?.id}>
-          <TaskCard task={done} />
+          {statusDone?.map((done, index) => (
+            <div key={done?.id}>
+              <TaskCard task={done} index={index} />
+            </div>
+          ))}
+          {provided.placeholder}
         </div>
-      ))}
-
-      <AddTask />
-    </div>
+      )}
+    </Droppable>
   );
 };
