ca59e1298981aca2a8ef8000824caba14f30a4d6
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-14 15:17:02 +0530

    feat: add drag and drop functionality

--- src/context/statusContext.js
+++ src/context/statusContext.js
@@ -4,16 +4,61 @@ import { useTask } from "./taskContext";
 const StatusContext = createContext();
 
 export const StatusProvider = ({ children }) => {
-  const { tasks } = useTask();
+  const { filteredTask } = useTask();
 
-  const statusReady = tasks?.filter((task) => task.status === "Ready");
-  const statusProgress = tasks?.filter((task) => task.status === "In Progress");
-  const statusTesting = tasks?.filter((task) => task.status === "Testing");
-  const statusDone = tasks?.filter((task) => task.status === "Done");
+  let statusReady = filteredTask?.filter((task) => task.status === "Ready");
+  let statusProgress = filteredTask?.filter(
+    (task) => task.status === "In Progress"
+  );
+  let statusTesting = filteredTask?.filter((task) => task.status === "Testing");
+  let statusDone = filteredTask?.filter((task) => task.status === "Done");
+
+  const onDragEnd = (result) => {
+    const { source, destination } = result;
+
+    if (!destination) return;
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+
+    let add;
+
+    if (source.droppableId === "ReadyTasks") {
+      add = statusReady[source.index];
+      statusReady.splice(source.index, 1);
+    } else if (source.droppableId === "ProgressTasks") {
+      add = statusProgress[source.index];
+      statusProgress.splice(source.index, 1);
+    } else if (source.droppableId === "TestingTasks") {
+      add = statusTesting[source.index];
+      statusTesting.splice(source.index, 1);
+    } else if (source.droppableId === "DoneTasks") {
+      add = statusDone[source.index];
+      statusDone.splice(source.index, 1);
+    }
+
+    if (destination.droppableId === "ReadyTasks") {
+      statusReady.splice(destination.index, 0, add);
+    } else if (destination.droppableId === "ProgressTasks") {
+      statusProgress.splice(destination.index, 0, add);
+    } else if (destination.droppableId === "TestingTasks") {
+      statusTesting.splice(destination.index, 0, add);
+    } else if (destination.droppableId === "DoneTasks") {
+      statusDone.splice(destination.index, 0, add);
+    }
+  };
 
   return (
     <StatusContext.Provider
-      value={{ statusReady, statusProgress, statusTesting, statusDone }}
+      value={{
+        statusReady,
+        statusProgress,
+        statusTesting,
+        statusDone,
+        onDragEnd,
+      }}
     >
       {children}
     </StatusContext.Provider>
