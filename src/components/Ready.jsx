a24bd81e34840e509b95c903ea8b0207719db533
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-14 15:15:54 +0530

    feat: add ready list

--- src/components/Ready.jsx
+++ src/components/Ready.jsx
@@ -1,25 +1,34 @@
+import { Droppable } from "react-beautiful-dnd";
 import { useStatus } from "../context/statusContext";
-import { AddTask } from "./AddTaskBtn";
 import { TaskCard } from "./TaskCard";
 
 export const Ready = () => {
   const { statusReady } = useStatus();
   return (
-    <div>
-      <h2 className="border-b-4 border-gray-400 text-left pb-[15px] font-bold">
-        READY{" "}
-        <span className="text-gray-500 text-[12px] ">
-          ({statusReady?.length})
-        </span>
-      </h2>
+    <Droppable droppableId="ReadyTasks">
+      {(provided, snapshot) => (
+        <div
+          className={`w-[350px] ${
+            snapshot.isDraggingOver ? "bg-gray-300" : ""
+          } rounded-md`}
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+        >
+          <h2 className="border-b-4 border-gray-400 text-left pb-[15px] font-bold">
+            READY{" "}
+            <span className="text-gray-500 text-[12px] ">
+              ({statusReady?.length})
+            </span>
+          </h2>
 
-      {statusReady?.map((ready) => (
-        <div key={ready?.id}>
-          <TaskCard task={ready} />
+          {statusReady?.map((ready, index) => (
+            <div key={ready?.id}>
+              <TaskCard task={ready} index={index} />
+            </div>
+          ))}
+          {provided.placeholder}
         </div>
-      ))}
-
-      <AddTask />
-    </div>
+      )}
+    </Droppable>
   );
 };
