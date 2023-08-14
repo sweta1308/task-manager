a55756ebe7b4dc6f6dd3d625838a6d6cef11a133
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-14 17:16:27 +0530

    docs: add theme provider

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { TaskProvider } from "./context/taskContext";
 import { StatusProvider } from "./context/statusContext";
+import { ThemeProvider } from "./context/themeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-
-  <TaskProvider>
-    <StatusProvider>
-      <App />
-    </StatusProvider>
-  </TaskProvider>
+  <BrowserRouter>
+    <ThemeProvider>
+      <TaskProvider>
+        <StatusProvider>
+          <App />
+        </StatusProvider>
+      </TaskProvider>
+    </ThemeProvider>
+  </BrowserRouter>
 
   // </React.StrictMode>
 );
