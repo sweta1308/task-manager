d27a7746122d381c0e677171fc33822d4c86b391
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-14 15:17:53 +0530

    feat: add Bar chart in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { DragDropContext } from "react-beautiful-dnd";
 import { Done } from "../components/Done";
 import { Header } from "../components/Header";
 import { Links } from "../components/Links";
@@ -5,22 +6,29 @@ import { Options } from "../components/Options";
 import { Progress } from "../components/Progress";
 import { Ready } from "../components/Ready";
 import { Testing } from "../components/Testing";
+import { useStatus } from "../context/statusContext";
+import { BarChart } from "../components/BarChart";
 
 export const Home = () => {
+  const { onDragEnd } = useStatus();
   return (
     <>
-      <div className="shadow-md px-[40px] py-[30px] pb-0">
+      <div className="shadow-md px-[40px] py-[30px] pb-0 sm:px-[20px]">
         <Header />
         <Links />
       </div>
       <div className="bg-gray-200 px-[40px] pb-[50px]">
         <Options />
-        <div className="flex w-full justify-between">
-          <Ready />
-          <Progress />
-          <Testing />
-          <Done />
-        </div>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <div className="flex w-full justify-between flex-wrap md:justify-center">
+            <Ready />
+            <Progress />
+            <Testing />
+            <Done />
+          </div>
+        </DragDropContext>
+
+        <BarChart />
       </div>
     </>
   );
