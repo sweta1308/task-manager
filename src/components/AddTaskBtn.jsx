3408afa8e432552604ec9594a80e0296b8a5ad38
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-14 15:14:26 +0530

    remove component

--- src/components/AddTaskBtn.jsx
+++
@@ -1,9 +0,0 @@
-export const AddTask = () => {
-  return (
-    <div>
-      <button className="text-[#10b981] font-bold rounded-sm w-[350px] bg-white shadow-md py-[10px] text-[18px]">
-        <i className="fa-solid fa-plus"></i> Add Task
-      </button>
-    </div>
-  );
-};
