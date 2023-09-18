d26d9c7cd04b762f879856093965650da1e30ae4
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-09-18 21:09:29 +0530

    feat: updated header content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,30 @@
+import { Search } from "@mui/icons-material";
 import { useTheme } from "../context/themeContext";
+import { useTask } from "../context/taskContext";
 
 export const Header = () => {
   const { isDark, setIsDark } = useTheme();
+  const { filters, setFilters } = useTask();
+  const { searchInput } = filters;
   return (
     <>
-      <div className="flex items-center sm:flex-col sm:items-start">
-        <div className="flex items-center">
-          <i className="fa-solid fa-arrow-left-long border-2 p-[8px] cursor-pointer mr-[25px] text-[18px]"></i>
-          <div>
-            <h1 className="font-semibold text-[18px]">
-              Basic Structure Kanban Project
-            </h1>
-            <p className="text-[12px] text-gray-400 font-semibold">Paymo SRL</p>
-          </div>
-          <p className="text-[12px] bg-[#10b981] text-white px-[5px] rounded-lg self-start mt-[6px] mx-[15px] sm:mx-[5px]">
-            #BSKP
-          </p>
-        </div>
+      <div className="flex items-center justify-between px-[30px] sm:px-[10px]">
+        <h1 className="font-semibold text-[28px] text-[#33a37e]">
+          TaskManager
+        </h1>
 
+        <div className="shadow-xl p-[5px] rounded-md dark:bg-dark-light dark:shadow-white dark:shadow-sm md:hidden">
+          <Search />
+          <input
+            value={searchInput}
+            onChange={(e) =>
+              setFilters({ ...filters, searchInput: e.target.value })
+            }
+            placeholder="Search a task by task name or assignee..."
+            className="px-[10px] py-[5px] w-[400px] rounded-md text-[14px] outline-none dark:bg-dark-light md:w-[300px]"
+          />
+        </div>
         <div className="flex mt-[-15px] sm:mt-[5px]">
-          <p className="text-gray-400 self-start mt-[4px] text-[12px] border-2 py-[2px] px-[4px] rounded-md">
-            <i className="fa-regular fa-image"></i> Completed
-          </p>
-          <i className="fa-solid fa-gear text-gray-400 self-start mt-[8px] mx-[15px]"></i>
-          <i className="fa-regular fa-bell text-[#10b981] self-start mt-[8px] mr-[15px]"></i>
-          <i className="fa-solid fa-ellipsis text-gray-400 self-start mt-[8px] mr-[15px]"></i>
           {isDark ? (
             <i
               onClick={() => setIsDark((prev) => !prev)}
@@ -37,6 +37,17 @@ export const Header = () => {
             ></i>
           )}
         </div>
+      </div>
+      <div className="hidden mt-[20px] shadow-xl p-[5px] rounded-md dark:bg-dark-light dark:shadow-white dark:shadow-sm md:block">
+        <Search />
+        <input
+          value={searchInput}
+          onChange={(e) =>
+            setFilters({ ...filters, searchInput: e.target.value })
+          }
+          placeholder="Search a task by task name or assignee..."
+          className="px-[10px] py-[5px] w-[400px] rounded-md text-[14px] outline-none dark:bg-dark-light md:w-[300px] xs:w-[250px]"
+        />
       </div>
     </>
   );
